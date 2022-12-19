51a1948e8b3ee0d28a1e2a79bee224de0ecdb497
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-12-19 14:32:37 +0100

    first commit

---
+++ src/main/java/me/code/fulldemo/FullDemoApplication.java
@@ -0,0 +1,13 @@
+package me.code.fulldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullDemoApplication.class, args);
+    }
+
+}
